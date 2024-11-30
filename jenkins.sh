b9480ed32180c5d0ed384eb716c39f09eb5a8842
SrivalliAvala <srivalliavala999@gmail.com>
2024-11-30 12:34:54 +0530

    jenkins-setup

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,5 @@
 
 #!/bin/bash
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-17-openjdk jenkins -y
 
 #resize disk from 20GB to 50GB
 growpart /dev/nvme0n1 4
@@ -14,6 +11,11 @@ lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
 xfs_growfs /
 xfs_growfs /var/tmp
 xfs_growfs /var
+
+curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install fontconfig java-17-openjdk -y
+yum install jenkins -y
 
 systemctl daemon-reload
 systemctl enable jenkins
